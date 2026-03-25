42a39c65323702bc30a75978ccfce5fd35a8d7c9
marsukulele <mhschrag@syr.edu>
2026-03-25 17:57:24 +0000

    lab6 3

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -7,3 +7,24 @@ init_chat_model('claude-haiku-4-5-20251001')
 
 st.title('Lab 6: Movie Recommendation Chatbot')
 
+st.sidebar.selectbox('Select a movie genre:', ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi'], key='genre')
+st.sidebar.selectbox('Select a mood:', ['Happy', 'Sad', 'Excited', 'Relaxed'], key='mood')
+st.sidebar.selectbox('Select a persona:',  ['Film Critic', 'Casual Friend', 'Movie Journalist'], key='persona')
+
+PromptTemplate(
+    input_variables=['genre', 'mood', 'persona'],
+    template=(
+        "You are a {persona} who loves movies. Recommend 3 movies that fit the below criteria:\n"
+        "- Genre: {genre}\n"
+        "- Mood: {mood}\n"
+        "Please provide a brief explanation for your recommendations."
+    )
+).format(
+    genre=st.session_state.genre,
+    mood=st.session_state.mood,
+    persona=st.session_state.persona
+)
+
+#part b step 3 is next
+
+
