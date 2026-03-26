b1844196840d4951812c70430d81cb77913c1e62
marsukulele <mhschrag@syr.edu>
2026-03-26 19:53:29 +0000

    lab6 5

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -3,7 +3,7 @@ from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+init_chat_model("claude-haiku-4-5-20251001")
 
 st.title('Lab 6: Movie Recommendation Chatbot')
 
