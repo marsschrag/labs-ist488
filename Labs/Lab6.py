60c97c33c07b061128e887b7633f41e42bbe5284
marsukulele <mhschrag@syr.edu>
2026-03-26 19:54:55 +0000

    lab6 6

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -3,7 +3,7 @@ from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-init_chat_model("claude-haiku-4-5-20251001")
+llm = init_chat_model("claude-haiku-4-5-20251001", model_provider="anthropic")
 
 st.title('Lab 6: Movie Recommendation Chatbot')
 
