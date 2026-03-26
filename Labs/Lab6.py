57f9bbe70c63c1904ced1b0d71b3665dd575a1b2
marsukulele <mhschrag@syr.edu>
2026-03-26 19:51:08 +0000

    lab6 4

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -3,7 +3,7 @@ from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-init_chat_model('claude-haiku-4-5-20251001')
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 st.title('Lab 6: Movie Recommendation Chatbot')
 
@@ -11,7 +11,7 @@ st.sidebar.selectbox('Select a movie genre:', ['Action', 'Comedy', 'Drama', 'Hor
 st.sidebar.selectbox('Select a mood:', ['Happy', 'Sad', 'Excited', 'Relaxed'], key='mood')
 st.sidebar.selectbox('Select a persona:',  ['Film Critic', 'Casual Friend', 'Movie Journalist'], key='persona')
 
-PromptTemplate(
+template = PromptTemplate(
     input_variables=['genre', 'mood', 'persona'],
     template=(
         "You are a {persona} who loves movies. Recommend 3 movies that fit the below criteria:\n"
@@ -19,12 +19,38 @@ PromptTemplate(
         "- Mood: {mood}\n"
         "Please provide a brief explanation for your recommendations."
     )
-).format(
-    genre=st.session_state.genre,
-    mood=st.session_state.mood,
-    persona=st.session_state.persona
 )
 
-#part b step 3 is next
+chain = template | llm | StrOutputParser()
 
+if "last_recommendation" not in st.session_state:
+    st.session_state.last_recommendation = None
+
+if st.button('Get Movie Recommendations'):
+    st.session_state.last_recommendation = chain.invoke({
+        "genre": st.session_state.genre,
+        "mood": st.session_state.mood,
+        "persona": st.session_state.persona
+    })
+    st.write(st.session_state.last_recommendation)
+
+st.divider()
+follow_up = st.text_input('Ask a follow-up question about these movies:')
+
+followup_prompt = PromptTemplate(  
+    input_variables=['recommendations', 'question'],
+    template="Based on the following movie recommendations:\n{recommendations}\nAnswer the following question: {question}"
+)
+
+followup_chain = followup_prompt | llm | StrOutputParser()
+
+if follow_up:
+    if not st.session_state.last_recommendation:
+        st.warning('Please get movie recommendations first!')
+    else:
+        answer = followup_chain.invoke({
+            "recommendations": st.session_state.last_recommendation,
+            "question": follow_up
+        })
+        st.write(answer)
 
